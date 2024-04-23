15b46985f362b812282cb47d234cac94c482d2b2
mehedi008h <mehedi08h@gmail.com>
2024-04-23 08:19:35 +0600

    change password form validation with formik & yup

--- app/dashboard/change-password/page.tsx
+++ app/dashboard/change-password/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+
 import { HorizontalLine } from "@/components/common";
 import { ChangePasswordField } from "@/components/dashboard";
 import React from "react";
 import * as yup from "yup";
+import { Formik } from "formik";
+
+// min 8 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
+// schema for validation
+const ChangePasswordSchema = yup
+    .object({
+        currentPassword: yup
+            .string()
+            .min(
+                8,
+                "Current Password must contain 8 or more characters with at least one of each: uppercase, lowercase, number!"
+            )
+            .matches(passwordRules, {
+                message: "Please create a stronger password",
+            })
+            .required("Current Password is required!"),
+        newPassword: yup
+            .string()
+            .min(
+                8,
+                "New Password must contain 8 or more characters with at least one of each: uppercase, lowercase, number!"
+            )
+            .matches(passwordRules, {
+                message: "Please create a stronger password",
+            })
+            .required("New password is required!"),
+        confirmPassword: yup
+            .string()
+            .min(
+                8,
+                "Confirm Password must contain 8 or more characters with at least one of each: uppercase, lowercase, number!"
+            )
+            .matches(passwordRules, {
+                message: "Please create a stronger password",
+            })
+            .oneOf(
+                [yup.ref("newPassword")],
+                "Confirm Passwords must match with New Password!"
+            )
+            .required("Confirm Password is required!"),
+    })
+    .required();
 
 const ChangePasswordPage = () => {
     return (
@@ -10,45 +56,100 @@ const ChangePasswordPage = () => {
             <h5 className="font-normal text-base text-neutral-500">
                 Please enter your current password to change your password.
             </h5>
+            <Formik
+                initialValues={{
+                    currentPassword: "",
+                    newPassword: "",
+                    confirmPassword: "",
+                }}
+                validationSchema={ChangePasswordSchema}
+                onSubmit={(values) => {
+                    // same shape as initial values
+                    console.log(values);
+                }}
+            >
+                {({
+                    errors,
+                    touched,
+                    values,
+                    handleSubmit,
+                    handleBlur,
+                    handleChange,
+                }) => (
+                    <form noValidate onSubmit={handleSubmit}>
+                        {/* current password  */}
+                        <ChangePasswordField
+                            label="Current Password"
+                            id="currentPassword"
+                            disabled={false}
+                            error={errors.currentPassword}
+                            required
+                            value={values.currentPassword}
+                            handleBlur={handleBlur}
+                            handleChange={handleChange}
+                            errorMessage={
+                                errors.currentPassword &&
+                                touched.currentPassword
+                                    ? errors.currentPassword
+                                    : null
+                            }
+                        />
+                        <HorizontalLine className="bg-neutral-200" />
 
-            {/* current password  */}
-            <ChangePasswordField
-                label="Current Password"
-                id="currentPassword"
-                name="currentPassword"
-            />
-            <HorizontalLine className="bg-neutral-200" />
+                        {/* new password  */}
+                        <ChangePasswordField
+                            label="New Password"
+                            id="newPassword"
+                            disabled={false}
+                            error={errors.newPassword}
+                            required
+                            value={values.newPassword}
+                            handleBlur={handleBlur}
+                            handleChange={handleChange}
+                            errorMessage={
+                                errors.newPassword && touched.newPassword
+                                    ? errors.newPassword
+                                    : null
+                            }
+                        />
+                        <HorizontalLine className="bg-neutral-200" />
 
-            {/* new password  */}
-            <ChangePasswordField
-                label="New Password"
-                id="newPassword"
-                name="newPassword"
-            />
-            <HorizontalLine className="bg-neutral-200" />
+                        {/* new password  */}
+                        <ChangePasswordField
+                            label="Confirm Password"
+                            id="confirmPassword"
+                            disabled={false}
+                            error={errors.confirmPassword}
+                            required
+                            value={values.confirmPassword}
+                            handleBlur={handleBlur}
+                            handleChange={handleChange}
+                            errorMessage={
+                                errors.confirmPassword &&
+                                touched.confirmPassword
+                                    ? errors.confirmPassword
+                                    : null
+                            }
+                        />
 
-            {/* new password  */}
-            <ChangePasswordField
-                label="Confirm Password"
-                id="confirmPassword"
-                name="confirmPassword"
-            />
-
-            {/* button  */}
-            <div className="w-full flex justify-end gap-5 mt-10">
-                <button
-                    type="reset"
-                    className="w-fit flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-neutral-700 bg-transparent border-neutral-500 focus:outline-none"
-                >
-                    Reset
-                </button>
-                <button
-                    type="submit"
-                    className="w-fit flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
-                >
-                    Change Password
-                </button>
-            </div>
+                        {/* button  */}
+                        <div className="w-full flex justify-end gap-5 mt-10">
+                            <button
+                                type="reset"
+                                className="w-fit flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-neutral-700 bg-transparent border-neutral-500 focus:outline-none"
+                            >
+                                Reset
+                            </button>
+                            <button
+                                type="submit"
+                                className="w-fit flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
+                            >
+                                Change Password
+                            </button>
+                        </div>
+                    </form>
+                )}
+            </Formik>
         </div>
     );
 };
--- components/dashboard/ChangePasswordField.tsx
+++ components/dashboard/ChangePasswordField.tsx
@@ -6,10 +6,26 @@ import { LuEye, LuEyeOff } from "react-icons/lu";
 interface Props {
     label: string;
     id: string;
-    name: string;
+    disabled?: boolean;
+    required?: boolean;
+    error?: string;
+    value?: string;
+    handleBlur: any;
+    handleChange: any;
+    errorMessage?: string | null;
 }
 
-const ChangePasswordField = ({ label, id, name }: Props) => {
+const ChangePasswordField = ({
+    label,
+    id,
+    disabled = false,
+    required,
+    value,
+    handleBlur,
+    handleChange,
+    error,
+    errorMessage,
+}: Props) => {
     const [togglePassword, setTooglePassword] = useState<boolean>(false);
 
     // toggle password view
@@ -18,6 +34,7 @@ const ChangePasswordField = ({ label, id, name }: Props) => {
     };
     return (
         <div className="my-8 grid grid-cols-12 items-center">
+            {/* label  */}
             <div className="col-span-4">
                 <label
                     htmlFor={id}
@@ -26,28 +43,46 @@ const ChangePasswordField = ({ label, id, name }: Props) => {
                     {label}
                 </label>
             </div>
+
+            {/* input field & error message */}
             <div className="col-span-4 relative">
                 <input
-                    type={togglePassword ? "text" : "password"}
-                    name={name}
                     id={id}
-                    autoComplete="current-password"
-                    className="px-2 focus:ring-indigo-500 focus:border-indigo-500 w-full h-10 sm:text-sm border-neutral-400 border-[1px] rounded-md outline-none"
+                    type={togglePassword ? "text" : "password"}
+                    disabled={disabled}
+                    required={required}
+                    value={value}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    className={`px-2  w-full h-10 sm:text-sm border-[1px] rounded-md outline-none ${
+                        error
+                            ? "border-red-500 focus:ring-red-500 focus:border-red-500"
+                            : "border-neutral-400 focus:ring-indigo-500 focus:border-indigo-500"
+                    }`}
                 />
 
                 {/* toogle icon  */}
                 {togglePassword ? (
+                    // close icon
                     <LuEyeOff
                         onClick={handleToogle}
                         className="absolute right-3 top-[10px] text-neutral-500 cursor-pointer"
                         size={20}
                     />
                 ) : (
+                    // view icon
                     <LuEye
                         onClick={handleToogle}
                         className="absolute right-3 top-[10px] text-neutral-500 cursor-pointer"
                         size={20}
                     />
+                )}
+
+                {/* error message  */}
+                {error && (
+                    <p className="text-red-500 mt-1 text-sm font-normal">
+                        {errorMessage}
+                    </p>
                 )}
             </div>
         </div>
