6fb6469ea04753689da002fbfcee80db306a8361
mehedi008h <mehedi08h@gmail.com>
2024-04-24 15:24:05 +0600

    design followed store page

---
+++ app/dashboard/followed-store/page.tsx
@@ -0,0 +1,12 @@
+import { Vendors } from "@/components/vendors";
+import React from "react";
+
+const FollowedStore = () => {
+    return (
+        <div className="w-full h-full p-5 bg-neutral-50 rounded-md shadow-md">
+            <Vendors />
+        </div>
+    );
+};
+
+export default FollowedStore;
